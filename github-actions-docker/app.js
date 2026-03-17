cc7909ccd18694e384f093be15b1228d5e8888fb
jagadeesh060696 <jagadeeshpower27@gmail.com>
2026-03-17 16:45:40 +0530

    my first commit production style

---
+++ github-actions-docker/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Hello World! This is a production-ready Docker app.');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+
